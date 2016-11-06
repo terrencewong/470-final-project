e9a1a216ea259551309a0ac25aabb558511ac654
Ariana <arianaw@sfu.ca>
2016-11-06 15:20:34 -0800

    Small changes + branch creation

--- finalproject/restaurant/views.py
+++ finalproject/restaurant/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello Group 4: Here is the empty project site.")
+
+def server(request):
+    return HttpResponse("Hello Server: Here is your home page")
