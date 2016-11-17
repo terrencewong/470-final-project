6435662d47c50b1a7a1dd386c6c069e45fbbfacf
xinranzhang <xza116@sfu.ca>
2016-11-16 16:03:34 -0800

    factored User out from other model class

--- finalproject/restaurant/models.py
+++ finalproject/restaurant/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Order(models.Model):
-    user = models.ForeignKey(User)
     Code = models.CharField(max_length=20)
     Table = models.IntegerField(default=0)
     Completed = models.BooleanField(default=0)
@@ -14,3 +13,10 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
