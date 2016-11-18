6710b56da994796068934d288c8816a5fc387140
twa63 <twa63@sfu.ca>
2016-11-17 17:09:22 -0800

    models.py update

--- finalproject/restaurant/models.py
+++ finalproject/restaurant/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
+class Table (models.Model):
+	Table = models.IntegerField(default=0)
+	Code = models.CharField(max_length=20)	
+
 class Order(models.Model):
     Code = models.CharField(max_length=20)
     Table = models.IntegerField(default=0)
     Completed = models.BooleanField(default=0)
     StartTime = models.DateTimeField(default=timezone.now())
-
     def __str__(self):
-        return self.Table
+       return self.Table
+	
+class MenuItem(models.Model):
+	order = models.ForeignKey(Order)
+	name = models.CharField(max_length=200)
+	item_id = models.CharField(max_length=20)
+	price = models.IntegerField(default=0)
+	description = models.CharField(max_length=250)
+	def __str__(self):
+		return self.name
+   
