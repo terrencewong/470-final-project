87e5f651d1ea2b4c11486b64c56b11e34d8496e9
Zeng-Yuan (David) Lee <zylee@sfu.ca>
2016-11-30 00:14:50 -0800

    Uploaded my set-up for the production server

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -2,61 +2,120 @@
 cookbook_file "apt-sources.list" do
   path "/etc/apt/sources.list"
 end
+
 execute 'apt_update' do
   command 'apt-get update'
+end
+
+execute 'apt_upgrade' do
+  command 'apt-get upgrade -y'
 end
 
 # Base configuration recipe in Chef.
 package "wget"
 package "ntp"
+
 cookbook_file "ntp.conf" do
   path "/etc/ntp.conf"
 end
+
 execute 'ntp_restart' do
   command 'service ntp restart'
 end
 
-package 'python' do
-  action :install
-end
-execute 'python-setup' do
-  command 'sudo apt-get install python3'
-end
-cookbook_file "get-pip.py" do
-  path "/etc/get-pip.py"
-end
-execute 'pip-setup' do
-  command 'python3 /etc/get-pip.py'
+
+
+# Start of my additions to this configuration file:
+
+package "python3.5"
+package "libpython3.5-dev"
+package "python3.5-dev"
+package "python3-pip"
+
+execute 'install_django' do
+  command 'pip3 install Django'
 end
 
-execute 'git-setup' do
-  command 'sudo apt-get install git'
+
+
+package "postgresql"
+package "postgresql-server-dev-all"
+
+execute 'install_psycopg2' do     #Dependent on 'postgresql-server-dev-all'
+  command 'pip3 install psycopg2'
+  #ALTERNATIVELY: command apt-get install python3-psycopg2
 end
 
-execute 'django-setup' do
-  command 'git clone https://github.com/django/django.git'
-  command 'mkdir ~/.virtualenvs'
-  command 'sudo apt-get install python3-pip'
-  command 'sudo pip3 install virtualenv'
-  command 'virtualenv ~/.virtualenvs/djangodev'
-  command '. ~/.virtualenvs/djangodev/bin/activate'
-  command 'pip install django'
+execute 'create_postgresql_db' do
+  command 'echo "CREATE DATABASE mydb; CREATE USER ubuntu; GRANT ALL PRIVILEGES ON DATABASE mydb TO ubuntu;" | sudo -u postgres psql'
 end
 
-execute 'run-server' do
+
+
+package "libpcre3"
+package "libpcre3-dev"
+
+execute 'install_uwsgi' do
+  command 'pip3 install uwsgi'
+end
+
+cookbook_file "rc.local-default" do
+  path "/etc/rc.local"
+end
+
+execute 'start_uwsgi' do
+  command '/etc/rc.local'
+end
+
+
+
+package "nginx"
+
+cookbook_file "nginx-default" do
+  path "/etc/nginx/sites-available/default"
+end
+
+execute 'restart_nginx' do
+  command 'service nginx restart'
+end
+
+
+
+execute 'migrate_schema' do
   user 'ubuntu'
-  cwd '/home/ubuntu/project/finalproject'
-  command 'nohup python3 ./manage.py runserver 0.0.0.0:8080&'
+  cwd '/home/ubuntu/project/webroot'
+  command 'python3.5 manage.py migrate'
 end
 
-execute 'create-database' do
+#WHEN YOU ARE READY TO LOAD DATA INTO THE DATABASE, UNCOMMENT THIS BLOCK:
+#execute 'load_data_into_database' do
+#  user 'ubuntu'
+#  cwd '/home/ubuntu/project/webroot'
+#  command 'python3.5 manage.py loaddata dbdump.json'
+#end
+
+execute 'collect_static_files' do
   user 'ubuntu'
-  cwd '/home/ubuntu/project/finalproject'
-  command 'nohup python3 ./manage.py migrate'
+  cwd '/home/ubuntu/project/webroot'
+  command 'python3.5 manage.py collectstatic --noinput'
 end
 
-execute 'database-setup' do
-  user 'ubuntu'
-  cwd '/home/ubuntu/project/finalproject'
-  command 'nohup python3 ./manage.py loaddata initial_data.json'
-end
+
+
+#This is only used if running the web application using the development web server:
+#execute 'run_webserver' do
+#  user 'ubuntu'
+#  cwd '/home/ubuntu/project/webroot'
+#  command 'nohup python3.5 manage.py runserver 0.0.0.0:80 &'
+#end
+
+
+
+#python3.5 ~/project/webroot/manage.py dbshell
+#select * from polls_question;
+
+#sudo cat /var/log/uwsgi.log  <-- uWSGI log file (name and location as chosen by you).
+
+#sudo cat /var/log/postgresql/postgresql-9.5-main.log  <-- PostgresQL log file.
+
+#curl -i http://localhost:8000  <-- run from inside virtual machine ('vagrant ssh').
